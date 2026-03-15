b17cd618f6bf7752de98c1a415a151dcf29b4d07
eucah <128732369+eucah@users.noreply.github.com>
2026-03-15 14:53:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true, // Helps with older plugins
+    },
+  },
+  optimizeDeps: {
+    include: ["@yesprasoon/capacitor-bluetooth-communication", "@capacitor/core"],
+  },
+});
