a91f1e53e8f87d9e988912c2fb4afc37b84a27df
eucah <128732369+eucah@users.noreply.github.com>
2026-04-08 17:36:28 +0200

    Update ProjectHome.tsx

--- src/components/ProjectHome.tsx
+++ src/components/ProjectHome.tsx
@@ -5,7 +5,8 @@ import { ProjectData } from "@/types/project";
 import { parseProjectFile } from "@/lib/file-utils";
 import { checkAndRequestPermissions } from "@/lib/permissions";
 import { stopScanning } from "@/lib/bt-service";
-import titleLight from "@/assets/title-continuity-light.png";
+import titleLight from "../../assets/title-continuity-light.png";
+import titleDark from "../../assets/title-continuity-dark.png";
 
 export type AppRole = "master" | "viewer";
 
@@ -48,6 +49,10 @@ const ProjectHome = ({ onLoadProject, onCreateProject, onViewerScan, onHelp }: P
     <div className="h-screen bg-background flex flex-col items-center justify-center px-4 safe-area-all">
       <div className="w-full flex justify-center mb-1">
   <picture>
+    {/* Dark mode */}
+    <source srcSet={titleDark} media="(prefers-color-scheme: dark)" />
+
+    {/* Light mode */}
     <img
       src={titleLight}
       alt="Essais Continuité"
