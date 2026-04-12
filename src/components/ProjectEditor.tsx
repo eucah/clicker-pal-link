318bffe0e589304b001ee551f53b3ff63201fde7
eucah <128732369+eucah@users.noreply.github.com>
2026-04-12 15:23:05 +0200

    Fix button label position in EditorRow component

--- src/components/ProjectEditor.tsx
+++ src/components/ProjectEditor.tsx
@@ -115,8 +115,8 @@ interface EditorRowProps {
 
 const EditorRow = memo(({ index, info, onUpdate }: EditorRowProps) => (
   <tr className="border-b border-border/80">
-    <td className="py-0.5 font-bold text-foreground w-9">{getButtonLabel(index)}</td>
-    <td className="py-0.5 pr-1">
+    <td className="py-0.5 font-bold text-foreground w-9"></td>
+    <td className="py-0.5 pr-1">{getButtonLabel(index)}
       <Input
         value={info.fils}
         onChange={(e) => onUpdate(index, "fils", e.target.value)}
