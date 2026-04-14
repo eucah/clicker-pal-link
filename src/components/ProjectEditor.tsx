2c3bc7aca1742f4b422272a44b1642d8f86fd3dc
eucah <128732369+eucah@users.noreply.github.com>
2026-04-14 04:58:13 +0200

    Update ProjectEditor.tsx

--- src/components/ProjectEditor.tsx
+++ src/components/ProjectEditor.tsx
@@ -83,10 +83,10 @@ const ProjectEditor = ({ onSave, onAccess, onCancel }: ProjectEditorProps) => {
             <thead>
               <tr className="border-b border-border text-muted-foreground">
                 <th className="py-1 pl-1 text-left w-9">#</th>
-                <th className="py-1 pl-4 text-left text-blue-600">Fils</th>
-                <th className="py-1 pl-4 text-left text-green-600">Borne</th>
-                <th className="py-1 pl-4 text-left text-red-600">Bornier</th>
-                <th className="py-1 pl-4 text-left text-amber-700">Cf/Cm</th>
+                <th className="py-1 pl-3 text-left text-blue-600">Fils</th>
+                <th className="py-1 pl-3 text-left text-green-600">Borne</th>
+                <th className="py-1 pl-3 text-left text-red-600">Bornier</th>
+                <th className="py-1 pl-3 text-left text-amber-700">Cf/Cm</th>
                 <th className="py-1 text-center w-14 text-primary">Non Testé</th>
               </tr>
             </thead>
