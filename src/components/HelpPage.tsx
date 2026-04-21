4747a6640b36fb5cfd84d18bc582bffe9ed407b8
eucah <128732369+eucah@users.noreply.github.com>
2026-04-21 04:35:36 +0200

    Update HelpPage.tsx

--- src/components/HelpPage.tsx
+++ src/components/HelpPage.tsx
@@ -57,7 +57,7 @@ const HelpPage = ({ onBack }: HelpPageProps) => {
             <li>L'<strong>Observateur</strong> se connecte au Contrôleur pour visualiser l'état du projet en temps réel.</li>
             <li>Il ne peut pas modifier les états.</li>
             <li>Il a la possibilité de sélectionner un bouton pour avoir les informations (fils, bornes, borniers, Cf/Cm).</li>
-            <li>Commme le Contrôleur il a la possibilité d'éditer un Rapport.</li>
+            <li>Comme le Contrôleur il a la possibilité d'éditer un Rapport.</li>
           </ul>
         </section>
 
